f5b5ee4457a9eb18d19007f9c3d99ec1b22c18d3
A Sorokin <lptolik@gmail.com>
2015-11-02 21:01:05 +0300

    simplest possible merger which does not test edges for duplication and do not handle attributes.

---
+++ src/main/java/ru/psn/icb/promodel/biomedb/mergeNodes/IEdgeMergeCheck.java
@@ -0,0 +1,8 @@
+package ru.psn.icb.promodel.biomedb.mergeNodes;
+
+import org.neo4j.graphdb.Relationship;
+
+public interface IEdgeMergeCheck {
+	boolean canMerge(Relationship r1, Relationship r2);
+
+}
