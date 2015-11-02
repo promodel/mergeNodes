a667bce3f730a7f6331eaa1400209a4593baba4c
A Sorokin <lptolik@gmail.com>
2015-11-02 16:29:51 +0300

    default maven projec

---
+++ src/main/java/ru/psn/icb/promodel/biomedb/mergeNodes/App.java
@@ -0,0 +1,13 @@
+package ru.psn.icb.promodel.biomedb.mergeNodes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
